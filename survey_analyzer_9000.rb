3eec907f3a49172900719ed9b5fe19e1f6cfad73
oschebell <oschebell@gmail.com>
2016-08-30 16:19:59 +1000

    Add survey and response classes

--- survey_analyzer_9000.rb
+++ survey_analyzer_9000.rb
@@ -2,10 +2,10 @@ require_relative "lib/survey"
 
 loop do
   puts "Please enter the questions filename, including the .csv"
-  @questions_filename = gets.chomp #get input from user
+  questions_filename = gets.chomp #get input from user
 
-  if @questions_filename.include?(".csv")
-    @survey = Survey.new(@questions_filename)
+  if questions_filename.include?(".csv")
+    @survey = Survey.new(questions_filename)
     break
   else
     puts "That isn't a CSV file. Please try again."
